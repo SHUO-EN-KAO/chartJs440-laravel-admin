4d95dc35367a60c5e5aad8a6133a2ed15ba4d07f
Frank Kao <frankkao@softstargames.com.tw>
2023-09-07 18:53:36 +0800

    options settings

--- resources/views/chartJs440.blade.php
+++ resources/views/chartJs440.blade.php
@@ -9,7 +9,14 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <style>
-    #myChart{
+    #myChart {
+        background-color: #fff;
+        border-radius: 15px;
+        padding: 5px;
+        height: 245px;
+    }
+
+    #userNewLineChart {
         background-color: #fff;
         border-radius: 15px;
         padding: 5px;
@@ -18,7 +25,7 @@
 </style>
 
 <body>
-    <div>
+    <div style="margin-bottom: 10px">
         <canvas id="myChart"></canvas>
     </div>
 
@@ -40,10 +47,81 @@
                 }]
             },
             options: {
-                maintainAspectRatio:false,
+                maintainAspectRatio: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Example',
+                        font: {
+                            size: 18
+                        },
+                        padding: 0,
+                    },
+                    legend: true,
+                },
                 scales: {
                     y: {
-                        beginAtZero: true
+                        beginAtZero: true,
+                    }
+                }
+            }
+        });
+
+        const userNewCtx = document.getElementById('userNewLineChart');
+
+
+
+        new Chart(userNewCtx, {
+            type: 'line',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                        label: 'AndroidUserCount',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: 'transparent',
+                        borderColor: '#943F00',
+                        pointBorderColor: '#943F00',
+                        pointBackgroundColor: '#943F00',
+                    },
+                    {
+                        label: 'iOSUserCount',
+                        data: [88, 77, 33, 44, 55, 66],
+                        backgroundColor: 'transparent',
+                        borderColor: '#008391',
+                        pointBorderColor: '#008391',
+                        pointBackgroundColor: '#008391',
+                    },
+                ]
+            },
+            options: {
+                maintainAspectRatio: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'New User',
+                        font: {
+                            size: 18,
+                        },
+                        padding: 0,
+                    },
+                    legend: true,
+                },
+                scales: {
+                    x: {
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'hour',
+                            align: 'end',
+                        },
+                    },
+                    y: {
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'users',
+                            align: 'end',
+                        },
                     }
                 }
             }
