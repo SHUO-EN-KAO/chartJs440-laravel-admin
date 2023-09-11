a3d617bfa39034ea7d29820d9d7b0487df8cbd87
Frank Kao <frankkao@softstargames.com.tw>
2023-09-11 19:20:37 +0800

    Model settings

--- app/Models/NewUserApiData.php
+++ app/Models/NewUserApiData.php
@@ -10,4 +10,11 @@ class NewUserApiData extends Model
     use HasFactory;
 
     protected $table = 'new_user_api_data';
+
+    protected $fillable = [
+        'game_id',
+        'date',
+        'platform',
+        'user_count',
+    ];
 }
