1dd04a41b348b36a2c6a5c2786b9a5a04263f4db
Frank Kao <frankkao@softstargames.com.tw>
2023-09-12 17:34:08 +0800

    API值存入DB

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
 use Illuminate\Support\Facades\Http;
+use App\Models\NewUserApiData;
 
 class HomeController extends Controller
 {
@@ -45,10 +46,46 @@ class HomeController extends Controller
         $jsonNewUser = $response->json();
         // dd($jsonNewUser);
 
+        // 因為資料結構中statistics內為另一層陣列
+        // 所以獨立拉出另外定義後才能對其做foreach並存進DB
+        $newUserStatistics = $jsonNewUser['data']['statistics'];
+        // dd('newUserStatistics:', $newUserStatistics);
+
+        // 將API回傳值存入DB
+        foreach ($newUserStatistics as $data) {
+
+            // 查詢DB現有資料紀錄
+            $record = NewUserApiData::where([
+                'game_id' => $jsonNewUser['data']['id'],
+                'date' => $jsonNewUser['data']['date'],
+                'platform' => $data['platform']
+            ])->first();
+
+            // 若有資料則update無資料則create
+            if ($record) {
+                $record->update([
+                    'user_count' => json_encode($data['userCount'])
+                ]);
+            } else {
+                NewUserApiData::create([
+                    'game_id' => $jsonNewUser['data']['id'],
+                    'date' => $jsonNewUser['data']['date'],
+                    'platform' => $data['platform'],
+                    'user_count' => json_encode($data['userCount'])
+                ]);
+            }
+        }
+
+        // 從DB提取資料
+        $newUserDataFromDB = NewUserApiData::all();
+
         $content->title('testResult');
         $content->description('Today  : ' . date('Y-m-d'));
+
+        // 直接取用API傳值呈現view
         $content->view('chartJs440', [
             'jsonNewUser' => $jsonNewUser,
+            'newUserDataFromDB' => $newUserDataFromDB,
         ]);
 
         return $content;
