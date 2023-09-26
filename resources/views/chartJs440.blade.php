0ea5a5013109f2b4368d12f8bd2b01d63290af72
Frank Kao <frankkao@softstargames.com.tw>
2023-09-26 15:50:16 +0800

    Index view blade title chang

--- resources/views/chartJs440.blade.php
+++ resources/views/chartJs440.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>testResult</title>
+    <title>Index</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <style>
